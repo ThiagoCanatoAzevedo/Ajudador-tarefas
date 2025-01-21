7ced25267d96ae77f04c93cc8dbddb961eb6551f
Thiago Canato de Azevedo <97035376+ThiagoCanatoAzevedo@users.noreply.github.com>
2025-01-21 09:59:11 -0300

    Update Gerador_TextoArquivo.c

--- Gerador_TextoArquivo.c
+++ Gerador_TextoArquivo.c
@@ -1,56 +1,54 @@
-//Bibliotecas que são importadas:
 #include <stdio.h>
 #include <stdlib.h>
-#include <cs50.c>
+#include <string.h>
 
-//Cria a função "main":
 int main(void)
 {
-    // Cria variável ponteiro para o arquivo:
     FILE *pont_arq;
+    char pergunta;
+    char item[100];
 
-    //abrindo o arquivo com tipo de abertura w
     pont_arq = fopen("ListaParaFazer.txt", "w");
 
-    //Testando se o arquivo foi realmente criado:
-    if(pont_arq == NULL)
+    if (pont_arq == NULL)
     {
         printf("Erro na abertura do arquivo!\n");
         return 1;
     }
     printf("Um arquivo .txt foi criado no seu computador!\n");
 
-    //Laço "infinito":
-    while(1){
+    while (1)
+    {
+        printf("Quais itens serÃ£o adicionados: ");
+        fgets(item, sizeof(item), stdin);
 
-        //Pega os itens do usuário:
-        string item = get_string("","Quais itens serão adicionados: ");
+        item[strcspn(item, "\n")] = '\0';
 
-        //Usando fprintf para armazenar a string no arquivo:
         fprintf(pont_arq, "%s\n", item);
 
         printf("Dados gravados com sucesso!\n");
 
-        //Pede a letra (N/n ou S/s):
-        char pergunta = get_char("Quer parar ou não (S/s ou N/n): ");
+        printf("Quer parar ou nÃ£o (S/s ou N/n): ");
+        scanf(" %c", &pergunta); 
 
-        // Verifica a letra digitada pelo usuário:
-        if(pergunta == 'S' || pergunta == 's'){
-		  	printf("Parando o programa...\n");
+        while (getchar() != '\n');  
+
+        if (pergunta == 'S' || pergunta == 's')
+        {
+            printf("Parando o programa...\n");
             break;
         }
-        else if (pergunta == 'N' || pergunta == 'n'){
+        else if (pergunta == 'N' || pergunta == 'n')
+        {
             continue;
-
         }
-        else{
-            printf("Essa letra é inválida. Vamos parar o programa, porém, você pode ver tudo que escreveu no arquivo de texto criado!");
+        else
+        {
+            printf("Resposta invÃ¡lida. O programa serÃ¡ interrompido.\n");
             break;
         }
-
-
     }
 
-    //Usando fclose para fechar o arquivo:
     fclose(pont_arq);
+    return 0;
 }
